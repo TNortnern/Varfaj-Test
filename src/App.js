324245afcb410bb00094406ec7147af10d449d43
TNORTHERN <traynorthern@yahoo.com>
2020-07-20 18:34:59 -0500

    adding background image

--- src/App.js
+++ src/App.js
@@ -7,9 +7,17 @@ import SpeedControls from "./components/SpeedControls";
 
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen w-screen">
+    <div
+      style={{
+        backgroundImage:
+          "url(https://upload.wikimedia.org/wikipedia/commons/1/1a/Abstract-background-white-12.jpg)",
+      }}
+      className="flex items-center justify-center h-screen w-screen"
+    >
       <div>
-        <h1 className="text-3xl font-bold mb-4">Welcome to the countdown timer.</h1>
+        <h1 className="text-3xl font-bold mb-4">
+          Welcome to the countdown timer.
+        </h1>
         <TimerForm />
         <Timer />
         <SpeedControls />
