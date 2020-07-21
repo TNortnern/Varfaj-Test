1ada3888b7a3a9ae4b71736a0f83fe2896ca6af1
TNORTHERN <traynorthern@yahoo.com>
2020-07-20 19:32:34 -0500

    removing boilerplate code

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
-import testReducer from '../features/testSlice';
 import timerSlice from '../slices/timerSlice';
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
-    test: testReducer,
     timer: timerSlice,
   },
 });
